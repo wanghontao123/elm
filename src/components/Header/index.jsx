fa2d93b80bb2453fc06a870b35bc5104564e6075
wht <3054955228@qq.com>
2020-01-14 18:50:18 +0800

    '修改页面'

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { Icon } from 'antd'
 
 export default class extends Component {
     render() {
         const { left, right, mid, back, jump } = this.props
         return (
             <div className="header-top">
-                <p onClick={back}>{left}</p>
+                <p onClick={back}><Icon type={left} /></p>
                 <p>{mid}</p>
                 <p onClick={jump}>{right}</p>
             </div>
--- src/pages/home_search/index.jsx
+++ src/pages/home_search/index.jsx
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
 import './style.less'
+import Header from '@@/Header'
+import { Input, Button } from 'antd'
 
 export default  class index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            txt: '',
+            searchHistory: []
+        }
+    }
+    
+    getVal = e => {
+        this.setState({ val: e.target.value })
+    }
+
+    btn = () => {
+        const { val, searchHistory } = this.state
+        searchHistory.push(val)
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
+
+    }
+
     render() {
         return (
             <div className="search"> 
-                home_search
+                <Header 
+                    left='left'
+                    mid='搜索'
+                />
+                <section>
+                    <div className="section-input">
+                        <Input  
+                            placeholder="请输入商家或美食名称"
+                            onChange={this.getVal}
+                        /> 
+                        <Button 
+                            type="primary" 
+                            onClick={this.btn}>
+                                提交
+                        </Button>
+                    </div>
+                    <div className="hide-title">
+                        <p>搜索历史</p>
+                        <p>清空搜索历史</p>
+                    </div>
+                </section>
             </div>
         );
     }
